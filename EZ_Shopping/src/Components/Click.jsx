19216d515e2a6edc2fce61ad1e45dde073c93b17
FOÏS Grégory <foisgregory38@gmail.com>
2023-02-14 15:53:30 +0100

    navigation

---
+++ EZ_Shopping/src/Components/Click.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+
+function Click() {
+  return (
+    <div>
+      <Button variant="secondary">Click</Button>
+    </div>
+  )
+}
+
+export default Click
--- EZ_Shopping/src/Components/Navigation.jsx
+++ EZ_Shopping/src/Components/Navigation.jsx
@@ -1,9 +1,30 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import { Container, Nav, Navbar, NavDropdown } from 'react-bootstrap';
 
 function Navigation() {
     return (
         <div>
+            <Navbar bg="light" expand="lg">
+                <Container>
+                    <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+                    <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="/">Home</Nav.Link>
+                            <Nav.Link href="/Basket">Basket</Nav.Link>
+                            <NavDropdown title="Theme" id="basic-nav-dropdown">
+                                <NavDropdown.Item>Dark</NavDropdown.Item>
+                                <NavDropdown.Item>Light</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+
+
+{/* 
+
             <h1>Navigation</h1>
             <NavLink to="/User">
                 Go to User page
@@ -15,7 +36,7 @@ function Navigation() {
             <br />
             <NavLink to="/">
                 Go to Home page
-            </NavLink>
+            </NavLink> */}
         </div>
 
     )
